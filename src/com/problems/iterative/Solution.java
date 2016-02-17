3be2452e365ad40aa4070f2dc0983e8b1acdf631
shravansabavat <shravan.sabavat@gmail.com>
2016-02-16 23:31:52 -0500

    find words solution

--- src/com/problems/iterative/Solution.java
+++ src/com/problems/iterative/Solution.java
@@ -18,29 +18,28 @@ import java.util.Set;
  * in validWords.txt under the same level of this class.
  * 
  * 3. A list of block of words is supplied at runtime as a system input to this program to search for valid words from
- * the valid-words.txt file. For example
- * 									TAMHAT(enter)
- * 									AIRQDR(enter)
- * 									MONCRT(enter)
- * 									EDRTCR(enter)
- * 									(enter) - And the last line should be an empty for the input to exit
- * 
+ * the valid-words.txt file. Copy paste this block and click enter 2 times
+TAMHAT
+AIRQDR
+MONCRT
+EDRTCR						
+ * (Even the empty line above)
  * @author ssabavat
  *
  */
 public class Solution {
-	
+
 	private static final String LINE_SEP = System.getProperty("line.separator", "\r\n");
-	
-    static Set<String> validWords = new HashSet<>();
-    
-    static Set<String> wordsFound = new HashSet<>();
-    
-    static char[][] twoDimCharArray = null;
-    
-    public static void main(String[] args) throws IOException {
+
+	private static Set<String> validWordsDictionary = new HashSet<>();
+
+	private static Set<String> wordsFoundInBlock = new HashSet<>();
+
+	private static char[][] twoDimCharArray = null;
+
+	public static void main(String[] args) throws IOException {
 		StringBuilder wordsBlock = new StringBuilder();
-		
+
 		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in)); 
 		String line;
 		while ((line = bufferReader.readLine()) != null && line.length()!= 0) {//would break when there is an empty input line
@@ -51,25 +50,42 @@ public class Solution {
 		solution.loadValidWords("valid-words.txt");
 		solution.convertWordsBlockInto2dArr(wordsBlock);
 		solution.findWords();
+
+		if (wordsFoundInBlock.size() > 0) {
+			System.out.println(wordsFoundInBlock);
+		} else {
+			System.out.println(
+					"There are not words found following this traversal at each letter, "
+					+ "left to right, right to left, down to up, up to down,"
+					+ "traverse up-left diagonal, up-right diagonal, down-left diagonal, down-right diagonal"
+					);
+		}
 		
-        System.out.println(wordsFound);
 	}
-    
+
+	/*
+	 * Loads valid words from a file. Please change it accordingly for testing.
+	 */
 	private void loadValidWords(String validWordsLocation) {
-	    try {
-	    	InputStream in = getClass().getResourceAsStream(validWordsLocation);
-	        BufferedReader bufferReader=new BufferedReader(new InputStreamReader(in));
+		try {
+			InputStream in = getClass().getResourceAsStream(validWordsLocation);
+			BufferedReader bufferReader=new BufferedReader(new InputStreamReader(in));
 			String line;
 			while ((line = bufferReader.readLine()) != null) {
-				validWords.add(line);
+				validWordsDictionary.add(line);
 			}
 			bufferReader.close();
 		} catch (Exception e) {
 			throw new RuntimeException("There was an error reading the file which has valid words.");
 		}
 	}
-    
-    private void convertWordsBlockInto2dArr(StringBuilder wordBlocks) {
+
+	/**
+	 * Converts blocks of strings into 2d char array.
+	 * 
+	 * @param wordBlocks
+	 */
+	private void convertWordsBlockInto2dArr(StringBuilder wordBlocks) {
 		String[] lines = wordBlocks.toString().split(LINE_SEP);
 
 		twoDimCharArray = new char[lines.length][lines[0].length()];
@@ -79,86 +95,97 @@ public class Solution {
 			twoDimCharArray[i] = line.toCharArray();
 		}
 	}
-    
-    private void findWords() {
-    	for (int i = 0; i < twoDimCharArray.length; i++) {
-            for (int j = 0; j < twoDimCharArray[0].length; j++) {
-                findWordsStartingWith(twoDimCharArray[i][j], i, j);
-            }
-        }
+
+	/**
+	 * Iterate over 2d array to find the possible words that match the valid words list.
+	 * 
+	 */
+	private void findWords() {
+		for (int i = 0; i < twoDimCharArray.length; i++) {
+			for (int j = 0; j < twoDimCharArray[0].length; j++) {
+				findWordsStartingWith(twoDimCharArray[i][j], i, j);
+			}
+		}
 	}
 
-    private static void findWordsStartingWith(char c, int i, int j) {
-        //traverse left
-        StringBuilder tempWord = new StringBuilder(String.valueOf(c));
-        for (int k = j - 1; k >= 0; k--) {
-            if (validWords.contains(tempWord.append(twoDimCharArray[i][k]).toString())) {
-                wordsFound.add(tempWord.toString());
-            }
-        }
-        
-        //traverse right
-        tempWord = new StringBuilder(String.valueOf(c));
-        for (int k = j + 1; k < twoDimCharArray[i].length; k++) {
-            if (validWords.contains(tempWord.append(twoDimCharArray[i][k]).toString())) {
-                wordsFound.add(tempWord.toString());
-            }
-        }
-        
-        //traverse up
-        tempWord = new StringBuilder(String.valueOf(c));
-        for (int k = i - 1; k >= 0; k--) {
-            if (validWords.contains(tempWord.append(twoDimCharArray[k][j]).toString())) {
-                wordsFound.add(tempWord.toString());
-            }
-        }
+	/**
+	 * Find matching at starting with a character, iterate over all possible paths (as mentioned in assumptions).
+	 * 
+	 * @param c - starting character.
+	 * @param i - row position of the character
+	 * @param j - col position of the character
+	 */
+	private static void findWordsStartingWith(char c, int i, int j) {
+		//traverse left
+		StringBuilder tempWord = new StringBuilder(String.valueOf(c));
+		for (int k = j - 1; k >= 0; k--) {
+			if (validWordsDictionary.contains(tempWord.append(twoDimCharArray[i][k]).toString())) {
+				wordsFoundInBlock.add(tempWord.toString());
+			}
+		}
 
-        //traverse down
-        tempWord = new StringBuilder(String.valueOf(c));
-        for (int k = i + 1; k < twoDimCharArray.length; k++) {
-            if (validWords.contains(tempWord.append(twoDimCharArray[k][j]).toString())) {
-                wordsFound.add(tempWord.toString());
-            }
-        }
-        
-        //traverse up-left diagonal
-        tempWord = new StringBuilder(String.valueOf(c));
-        int k = i-1;
-        int l = j-1;
-        while (k >= 0 && l >= 0) {
-            if (validWords.contains(tempWord.append(twoDimCharArray[k--][l--]).toString())) {
-                wordsFound.add(tempWord.toString());
-            }
-        }
+		//traverse right
+		tempWord = new StringBuilder(String.valueOf(c));
+		for (int k = j + 1; k < twoDimCharArray[i].length; k++) {
+			if (validWordsDictionary.contains(tempWord.append(twoDimCharArray[i][k]).toString())) {
+				wordsFoundInBlock.add(tempWord.toString());
+			}
+		}
 
-        //traverse up-right diagonal
-        tempWord = new StringBuilder(String.valueOf(c));
-        k = i-1;
-        l = j+1;
-        while (k >= 0 && l < twoDimCharArray[i].length) {
-            if (validWords.contains(tempWord.append(twoDimCharArray[k--][l++]).toString())) {
-                wordsFound.add(tempWord.toString());
-            }
-        }
+		//traverse up
+		tempWord = new StringBuilder(String.valueOf(c));
+		for (int k = i - 1; k >= 0; k--) {
+			if (validWordsDictionary.contains(tempWord.append(twoDimCharArray[k][j]).toString())) {
+				wordsFoundInBlock.add(tempWord.toString());
+			}
+		}
 
-        //traverse down-left diagonal
-        tempWord = new StringBuilder(String.valueOf(c));
-        k = i+1;
-        l = j-1;
-        while (k < twoDimCharArray.length && l >= 0) {
-            if (validWords.contains(tempWord.append(twoDimCharArray[k++][l--]).toString())) {
-                wordsFound.add(tempWord.toString());
-            }
-        }
-        
-        //traverse down-right diagonal
-        tempWord = new StringBuilder(String.valueOf(c));
-        k = i+1;
-        l = j+1;
-        while (k < twoDimCharArray.length && l < twoDimCharArray[i].length) {
-            if (validWords.contains(tempWord.append(twoDimCharArray[k++][l++]).toString())) {
-                wordsFound.add(tempWord.toString());
-            }
-        }
-    }
+		//traverse down
+		tempWord = new StringBuilder(String.valueOf(c));
+		for (int k = i + 1; k < twoDimCharArray.length; k++) {
+			if (validWordsDictionary.contains(tempWord.append(twoDimCharArray[k][j]).toString())) {
+				wordsFoundInBlock.add(tempWord.toString());
+			}
+		}
+
+		//traverse up-left diagonal
+		tempWord = new StringBuilder(String.valueOf(c));
+		int k = i-1;
+		int l = j-1;
+		while (k >= 0 && l >= 0) {
+			if (validWordsDictionary.contains(tempWord.append(twoDimCharArray[k--][l--]).toString())) {
+				wordsFoundInBlock.add(tempWord.toString());
+			}
+		}
+
+		//traverse up-right diagonal
+		tempWord = new StringBuilder(String.valueOf(c));
+		k = i-1;
+		l = j+1;
+		while (k >= 0 && l < twoDimCharArray[i].length) {
+			if (validWordsDictionary.contains(tempWord.append(twoDimCharArray[k--][l++]).toString())) {
+				wordsFoundInBlock.add(tempWord.toString());
+			}
+		}
+
+		//traverse down-left diagonal
+		tempWord = new StringBuilder(String.valueOf(c));
+		k = i+1;
+		l = j-1;
+		while (k < twoDimCharArray.length && l >= 0) {
+			if (validWordsDictionary.contains(tempWord.append(twoDimCharArray[k++][l--]).toString())) {
+				wordsFoundInBlock.add(tempWord.toString());
+			}
+		}
+
+		//traverse down-right diagonal
+		tempWord = new StringBuilder(String.valueOf(c));
+		k = i+1;
+		l = j+1;
+		while (k < twoDimCharArray.length && l < twoDimCharArray[i].length) {
+			if (validWordsDictionary.contains(tempWord.append(twoDimCharArray[k++][l++]).toString())) {
+				wordsFoundInBlock.add(tempWord.toString());
+			}
+		}
+	}
 }
